f3dd476852c3130f585c1347249475d2370226e8
Ronald Arias <ariasron@hotmail.com>
2021-05-30 13:32:18 -0500

    Adds function on order controller to add the total amount on stripe, key secret is needed

--- api/order/controllers/order.js
+++ api/order/controllers/order.js
@@ -1,8 +1,23 @@
 'use strict';
+const stripe = require('stripe')('sk_test_secret_key')
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  create: async ctx => {
+    const {name, total, items, stripeTokenId} = ctx.request.body;
+    const {id, username} = ctx.state.user;
+    const charge = await stripe.charges.create({
+      amount: total * 100,
+      currency: 'usd',
+      description: `Order ${new Date()} by ${username}`,
+      source: stripeTokenId
+    });
+
+    const order = await strapi.services.order.create({name, total, items, user: id});
+    return order;
+  }
+};
